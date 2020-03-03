acce5a647ff9d13f24c8e4bcb0fda1cd23bbd172
Mika Octo Frentzen <mika.leonheart6@gmail.com>
2020-03-03 17:33:58 +0700

    Bugfix Module UI integrate to app module :)

--- ui/src/main/java/dynamia/com/ui/NormalInputLayout.kt
+++ ui/src/main/java/dynamia/com/ui/NormalInputLayout.kt
@@ -5,15 +5,33 @@ import android.text.InputType
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.appcompat.widget.LinearLayoutCompat
 import kotlinx.android.synthetic.main.normal_input_layout.view.*
 
 
-class NormalInputLayout @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr){
-    init {
+class NormalInputLayout : LinearLayoutCompat{
+
+    private var attrs: AttributeSet? = null
+    private var defStyleAttr: Int = 0
+
+    constructor(ctx: Context) : super(ctx)
+
+    constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs) {
+        this.attrs = attrs
+        setAttributes(ctx)
+    }
+
+    constructor(ctx: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        ctx,
+        attrs,
+        defStyleAttr
+    ) {
+        this.attrs = attrs
+        this.defStyleAttr = defStyleAttr
+        setAttributes(ctx)
+    }
+
+    private fun setAttributes(ctx: Context) {
         val wrapContent = LayoutParams.WRAP_CONTENT
         val matchParent = LayoutParams.MATCH_PARENT
         layoutParams = LayoutParams(matchParent, wrapContent)
